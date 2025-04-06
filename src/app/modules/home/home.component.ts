c5533896a432d31daca6635a9ecc3190c92d2153
Karol <karol.gladosz@gmail.com>
2025-04-06 14:20:36 +0200

    home

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule],
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrls: ['./home.component.scss'],  // Poprawna właściwość
 })
 export class HomeComponent {
-  constructor() {}
 }
