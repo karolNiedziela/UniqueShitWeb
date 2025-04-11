659967d49c3adda0131698fd8bf2ca95c233835d
Karol <karol.gladosz@gmail.com>
2025-04-11 14:50:14 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './modules/header/header.component';
+import { FooterComponent } from "./modules/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   standalone: true,
 })
 export class AppComponent {
