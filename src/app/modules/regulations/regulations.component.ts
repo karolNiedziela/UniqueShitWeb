da5390857108167fe9efbbac8cbc94db1a19ec5e
Karol <karol.gladosz@gmail.com>
2025-04-11 14:53:25 +0200

    add regulations

--- src/app/modules/regulations/regulations.component.ts
+++ src/app/modules/regulations/regulations.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-regulations',
-  imports: [],
+  selector: 'app-contact',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './regulations.component.html',
   styleUrl: './regulations.component.scss'
 })
